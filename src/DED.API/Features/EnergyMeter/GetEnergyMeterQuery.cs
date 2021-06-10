54a20b18cea30ba0c4c36359e200ed9461107db1
aledefault <58753909+aledefault@users.noreply.github.com>
2021-06-10 15:53:02 +0200

    Updated some names on API

---
+++ src/DED.API/Features/EnergyMeter/GetEnergyMeterQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DED.API.Services;
+using MediatR;
+using MongoDB.Driver;
+
+namespace DED.API.Features.EnergyMeter
+{
+    public class GetEnergyMeterQuery
+    {
+        public record Query(Guid Id) : IRequest<Domain.EnergyMeter> { }
+
+        public class Handler : IRequestHandler<Query, Domain.EnergyMeter>
+        {
+            private readonly IMongoCollection<Domain.EnergyMeter> _collection;
+
+            public Handler(MongoDbService mongoService) => _collection = mongoService.GetCollection<Domain.EnergyMeter>();
+
+            public async Task<Domain.EnergyMeter> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var result = await _collection.FindAsync(device => device.Id == request.Id, cancellationToken: cancellationToken);
+                return await result.FirstOrDefaultAsync(cancellationToken);
+            }
+        }
+    }
+}
---
+++ src/DED.API/Features/WaterMeter/GetWaterMeterQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DED.API.Services;
+using MediatR;
+using MongoDB.Driver;
+
+namespace DED.API.Features.WaterMeter
+{
+    public class GetWaterMeterQuery
+    {
+        public record Query(Guid Id) : IRequest<Domain.WaterMeter> { }
+
+        public class Handler : IRequestHandler<Query, Domain.WaterMeter>
+        {
+            private readonly IMongoCollection<Domain.WaterMeter> _collection;
+
+            public Handler(MongoDbService mongoService) => _collection = mongoService.GetCollection<Domain.WaterMeter>();
+
+            public async Task<Domain.WaterMeter> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var result = await _collection.FindAsync(device => device.Id == request.Id, cancellationToken: cancellationToken);
+                return await result.FirstOrDefaultAsync(cancellationToken);
+            }
+        }
+    }
+}
